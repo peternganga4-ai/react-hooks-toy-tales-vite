d7e0fa333d06affbf4ff9e3738c3f2b7cfcb0614
Peter Ng'ang'a <peter.nganga4@student.moringaschool.com>
2026-08-14 20:38:19 +0300

    Add GET and POST toy functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,33 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+      .then((response) => response.json())
+      .then((data) => setToys(data));
+  }, []);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function handleAddToy(newToy) {
+    setToys((currentToys) => [...currentToys, newToy]);
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+
+      {showForm ? <ToyForm onAddToy={handleAddToy} /> : null}
+
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+
+      <ToyContainer toys={toys} />
     </>
   );
 }
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,24 +1,54 @@
 import React from "react";
 
-function ToyForm() {
+function ToyForm({ onAddToy }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const form = event.target;
+
+    const newToy = {
+      name: form.name.value,
+      image: form.image.value,
+      likes: 0,
+    };
+
+    fetch("http://localhost:3001/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newToy),
+    })
+      .then((response) => response.json())
+      .then((toy) => {
+        onAddToy(toy);
+        form.reset();
+      });
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
+
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
         />
+
         <br />
+
         <input
           type="text"
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
         />
+
         <br />
+
         <input
           type="submit"
           name="submit"
